c0cd2e35497f525cb1bf59380ed5e5b049108bd4
Maverick Tesini <mavericktesini@gmail.com>
2017-11-15 23:45:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
+<<<<<<< HEAD
 let companyCeo = 'Susan Smith';
+=======
+let companyCeo = 'Susan Smith';
+>>>>>>> 98f32aab31f8f627b5eae915f115edf9e67ff387
